7ab3e5cef4b83e9e797cc0e2d65acca1371dde21
llstrk <dryerr@gmail.com>
2016-04-22 00:36:10 +0200

    Added simple DSC

---
+++ DSC/Default.ps1
@@ -0,0 +1,12 @@
+configuration Default {
+	param (
+		[string]$ComputerName
+	)
+	
+	Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+
+	WindowsFeature TelnetClient {
+		Name = "Telnet-Client"
+		Ensure = "Present"
+	}
+}
