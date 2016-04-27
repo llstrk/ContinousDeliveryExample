1a58c76525b87b446754bbd2cec02a9f751109bb
llstrk <dryerr@gmail.com>
2016-04-27 11:12:41 +0200

    new dsc

--- DSC/Config.ps1
+++ DSC/Config.ps1
@@ -10,5 +10,11 @@ configuration Default {
             Name   = "Web-Server"
             Ensure = "Present"
         }
+        File WebSite {
+            DestinationPath = "C:\inetpub\iisstart.htm"
+            Contents  = "<html><p>DSC Agent ID: $((Get-DscLocalConfigurationManager).AgentID)</p><p>OS Version: $((Get-CimInstance -ClassName Win32_OperatingSystem).Version)</p></html>"
+            Ensure    = "Present"
+            DependsOn = "[WindowsFeature]WebServer"
+        }
     }
 }
